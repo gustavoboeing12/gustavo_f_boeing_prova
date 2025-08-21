5cc2ac2621b4b86586e2e22997690a452eca2eb5
gustavoboeing12 <gustavofratonib@gmail.com>
2025-08-21 17:01:38 -0300

    correção de bugs

--- MaterialApoio/MaterialApoio/validacoes.js
+++ MaterialApoio/MaterialApoio/validacoes.js
@@ -1,5 +1,5 @@
 function validarFuncionario() {
-    let nome = document.getElementById("nome_funcionario").value;
+    let nome = document.getElementById("nome").value;
     let telefone = document.getElementById("telefone").value;
     let email = document.getElementById("email").value;
 
